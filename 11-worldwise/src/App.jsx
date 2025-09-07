4c4ba6203a28835e0842b85a7ff98d320ea681d4
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2025-09-07 10:52:47 +0300

    feat: initializing new project -world-wise-

---
+++ 11-worldwise/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  const x = 2;
+  console.log(x);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
