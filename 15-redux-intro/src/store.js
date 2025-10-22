91dd773d57b8fff0165caebbbf04b12343908fde
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2025-10-22 17:46:31 +0300

    feat: itroducing redux-divtool

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const root = combineReducers({
   account: accountReducer,
@@ -9,6 +10,6 @@ const root = combineReducers({
 });
 
 // * applyMiddleware will enable dispatch function to accept function that will execute the side effect and then dispatch action whenever you want
-const store = createStore(root, applyMiddleware(thunk));
+const store = createStore(root, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
